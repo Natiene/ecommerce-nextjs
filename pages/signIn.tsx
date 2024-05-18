497ba1adcd40250ff77ae3241c5ca2e644ac4170
Natiene <cardosonatiene@gmail.com>
2024-05-18 17:05:46 -0300

    create Navbar

---
+++ pages/signIn.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type Props = {};
+
+const SignIn = (props: Props) => {
+
+    return (
+        <div>Hello World</div>
+    )
+
+}
+
+export default SignIn;
---
+++ pages/signUp.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type Props = {};
+
+const SignUp = (props: Props) => {
+
+    return (
+        <div></div>
+    )
+
+}
+
+export default SignUp;
